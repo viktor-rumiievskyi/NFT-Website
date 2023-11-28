49f9f3c42538ee9fc1eaf70dcea2d696922f9c15
viktor-rumiievskyi <vityok05042002@gmail.com>
2023-11-28 13:45:37 +0200

    Writing the Header part

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const elemToggleFunc = function (elem) {
+	elem.classList.toggle("active");
+}
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarToggleBtn = document.querySelector("[data-navbar-toggle-btn]");
+
+navbarToggleBtn.addEventListener("click", function () {
+	elemToggleFunc(navbar);
+});
